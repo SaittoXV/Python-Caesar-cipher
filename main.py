0599f67fd67d45b7bb80af3fd01b594b930d7bde
Abu Bakar Syed <syed.abubakar73@gmail.com>
2026-04-14 12:08:01 +0800

    change is_upper state type

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 user_message = input("Enter your message: ")
 user_shift = int(input("Enter your encyption number: "))
 encripted = []
-cap_state = "upper"
 
 for char in user_message:
     if char.isalpha():
-        if not char.isupper(): cap_state = "lower"
+        is_upper = char.isupper() 
         char = char.lower()
         sum_shift = ord(char) + user_shift 
         if sum_shift > 122:
             shift_number = 96 + (sum_shift - 122)
         else:
             shift_number = ord(char) + user_shift
-        if cap_state == "upper": shift_number = shift_number - 32
+        if is_upper: shift_number = shift_number - 32
         encripted.append(chr(shift_number))
-        cap_state = "upper"
     else:
         encripted.append(char)
         continue
-print(''.join(encripted))
+print("Caesar Cipher: ",''.join(encripted))
